1b78003c926201ae1dd8761a45ef760f802f8b64
Elijah Wigmore <elijahwigmore@gmail.com>
2017-10-15 22:32:03 +0000

    Displaying service listing IDs (client, service) on service listing index page

--- CSC444/app/controllers/service_listings_controller.rb
+++ CSC444/app/controllers/service_listings_controller.rb
@@ -1,20 +1,17 @@
 class ServiceListingsController < ApplicationController
   def index
+    @service_listings = ServiceListing.all
   end
 
+  # don't need now, maybe even never (Elijah)
   def new
   end
   
   def create
     @service_listing = ServiceListing.new(service_listing_params)
+    @service_listing.save
     
-    result = @service_listing.save
-    
-    if result
-      redirect_to client_path(@service_listing.clientID)
-    else
-      redirect_to client_path
-    end
+    redirect_to client_path(@service_listing.clientID)
 	end
 	
 	private

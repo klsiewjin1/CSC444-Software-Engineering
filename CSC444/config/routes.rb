818f803477747fa2514fbf156d1ac6c25a2595d2
Andrew <andrewj.allen@mail.utoronto.ca>
2017-10-16 20:22:41 +0000

    Add basic user sessions and login

--- CSC444/config/routes.rb
+++ CSC444/config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   root 'welcome#index'
- 
   get 'welcome/index'
   
+  # Signup page
   get 'signup', to: 'users#new'
   
-  resources :clients
+  # Logins and sessions
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :services
   resources :service_listings
-
   resources :teens
+  resources :clients
   resources :users
 end

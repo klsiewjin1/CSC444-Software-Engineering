e8dae2b6efc8cb4ce8d1d79cd83e703cc517cbec
AlexeiZeninCS <alexei.zenin@clientspectrum.com>
2017-10-15 20:01:34 -0400

    Added controller for reviews and initial empty views

---
+++ CSC444/app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+# Handles review access
+class ReviewsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end

f2e45ead0d131820b6ed5171bf07f4cf43fff758
Kung Lim Siew Jin <siewjin.kunglim@mail.utoronto.ca>
2017-10-19 17:35:10 -0400

    Added validations to user, changed usertype to a list

--- CSC444/app/models/user.rb
+++ CSC444/app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   validates :usertype, presence: true
-  validates :username, uniqueness:true
+  validates :username, uniqueness: true
+
+
+  validates :email, uniqueness: true, confirmation: true
+  validates :email_confirmation, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   
+  before_save :downcase_email
   has_secure_password
+
+  def downcase_email
+    self.email.downcase!
+  end
 end

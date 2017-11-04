ecfc2374345e743a4be0a6c850c98c0cf50ec68a
AlexeiZeninCSC444 <alexei.zenin@mail.utoronto.ca>
2017-11-04 15:51:04 -0400

    Finished Geocoder implementation. Now integrates with google api. Possible improvements would be to somehow ascertain if we get a wrong geocoordinate as the API will return a lat/long with addresses such as dog or blah which might not be desirable

--- CSC444/lib/shared/Geocoder.rb
+++ CSC444/lib/shared/Geocoder.rb
@@ -1,3 +1,6 @@
+require 'rest-client'
+require 'json'
+
 class Geocoder 
    
    # Returns a LatLong object with the coordinates of the address supplied. Address is just a string
@@ -7,15 +10,28 @@ class Geocoder
    		result = nil;
 
    		#do api call
-
+   		begin
+   		response = RestClient.get('https://maps.googleapis.com/maps/api/geocode/json', 
+   			{params: {address: address, key: 'AIzaSyAP_IzvS1mpKPgcs6meWzc6hu9PB0CVJFs'}});
+   		rescue RestClient::ExceptionWithResponse => err
+   			puts err.response;
+   			return result;
+		end
 
    		#set result of api call in return value
+   		jsonResp = JSON.parse(response.body);
+
+   		location = jsonResp['results'][0]['geometry']['location'];
+   		result = LatLong.new
+   		result.lat = location['lat'];
+   		result.long = location['lng'];
 
    		return result;
    end
 
 end
 
+# the data transfer object which contains a latitude and longitude
 class LatLong
 	attr_accessor :long
 	attr_accessor :lat

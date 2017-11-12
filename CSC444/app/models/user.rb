abbbdfbab95d9891c44b5e3781a0876f617174d4
andrewjallenuoft <andrewj.allen@mail.utoronto.ca>
2017-11-12 19:56:33 +0000

    Make registration form highlight and announce server side errors, e.g. non-unique username.

--- CSC444/app/models/user.rb
+++ CSC444/app/models/user.rb
@@ -3,25 +3,25 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   
   validates :fname, presence: true
-  validates_format_of :fname, with: /[-_.a-zA-Z0-9]{3,}/i, on: :create
+  validates_format_of :fname, with: /[-_.a-zA-Z0-9]{2,}/i, on: :create
   
   validates :lname, presence: true
-  validates_format_of :lname, with: /[-_.a-zA-Z0-9]{3,}/i, on: :create
+  validates_format_of :lname, with: /[-_.a-zA-Z0-9]{2,}/i, on: :create
   
   validates :address, presence: true
   validates_format_of :address, with: /([0-9]+)\s([a-z\s]+)/i, on: :create
   
   validates :state, presence: true
-  validates_format_of :state, with: /[a-z\s.]{3,}/i, on: :create
+  validates_format_of :state, with: /[a-z\s.]{2,}/i, on: :create
   
   validates :pcode, presence: true
   validates_format_of :pcode, with: /[0-9a-z]{3}\s?[0-9a-z]{2,3}/i, on: :create
   
   validates :city, presence: true
-  validates_format_of :city, with: /[a-z\s.]{3,}/i, on: :create
+  validates_format_of :city, with: /[a-z\s.]{2,}/i, on: :create
   
   validates :country, presence: true
-  validates_format_of :country, with: /[a-z\s.]{3,}/i, on: :create
+  validates_format_of :country, with: /[a-z\s.]{2,}/i, on: :create
   
   validates :cellphone, presence: true
   validates_format_of :cellphone, with: /[0-9]{10,}/i, on: :create
@@ -30,7 +30,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, confirmation: true
   validates :email_confirmation, presence: true
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates_format_of :email, with: /([\w.-]+)@([\w.-]+)\.([a-zA-Z.]{2,6})/i , on: :create
   
   before_save :downcase_email
   before_save :downcase_username

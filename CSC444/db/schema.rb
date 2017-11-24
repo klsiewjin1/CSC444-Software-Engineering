072052e7e9017773e3d657516535339dd887d91e
andrewjallenuoft <andrewj.allen@mail.utoronto.ca>
2017-11-24 22:52:03 +0000

    Fix registration bug where validation does not occur on blur when reaching page indirectly

--- CSC444/db/schema.rb
+++ CSC444/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171124184720) do
 
   create_table "notifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|

b071f405c58a9a069599fde7770f2bad5ea3dbae
AlexeiZeninCSC444 <alexei.zenin@mail.utoronto.ca>
2017-12-04 04:01:14 +0000

    Added production config for email

--- CSC444/config/environments/production.rb
+++ CSC444/config/environments/production.rb
@@ -91,5 +91,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Mailing host
-  config.action_mailer.default_url_options = { :host => "csc444-andrewallen.c9users.io" }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  host = 'teender.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end

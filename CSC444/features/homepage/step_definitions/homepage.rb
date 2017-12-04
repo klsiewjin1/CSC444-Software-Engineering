b027fb9fb0b5ece9569c9ea58b21d9ab3abd3fcd
Jeff Feng <jefff.feng@mail.utoronto.ca>
2017-12-04 15:29:57 +0000

    created initial homepage feature test file for cucumber, run with rake cucumber

---
+++ CSC444/features/homepage/step_definitions/homepage.rb
@@ -0,0 +1,19 @@
+Given("I am not logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the navbar") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the title and subtitles") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be able to login") do
+  pending # Write code here that turns the phrase above into concrete actions
+end

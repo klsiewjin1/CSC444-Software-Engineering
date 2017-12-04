ce9aca4fbc0cda8bb86008418366d7c98dc4c248
AlexeiZeninCSC444 <alexei.zenin@mail.utoronto.ca>
2017-12-04 04:47:02 +0000

    Clear the flash before showing activation success and in error

--- CSC444/app/controllers/account_activations_controller.rb
+++ CSC444/app/controllers/account_activations_controller.rb
@@ -15,6 +15,7 @@ class AccountActivationsController < ApplicationController
       flash[:success] = "Account activated!"
       redirect_to user
     else
+      flash.clear()
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
     end
